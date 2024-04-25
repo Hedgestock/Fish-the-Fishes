8bd2d427848fb94b04fbc0d792433f2a8c392611
Beewaffle <beetwinkies@gmail.com>
2024-04-25 16:02:12 +0200

    Final swordfish change I swear

--- Fish the fishes/Scenes/Game/Game.cs
+++ Fish the fishes/Scenes/Game/Game.cs
@@ -5,74 +5,74 @@ using System;
 
 public partial class Game : Node
 {
-	[Export]
-	public Array<PackedScene> Fishes { get; set; }
+    [Export]
+    public Array<PackedScene> Fishes { get; set; }
 
-	[Export]
-	public Array<PackedScene> Trashes { get; set; }
+    [Export]
+    public Array<PackedScene> Trashes { get; set; }
 
-	private GameManager GM;
+    private GameManager GM;
 
-	public enum Mode
-	{
-		Classic,
-		GoGreen,
-		Target,
-		Training,
-		TimeAttack,
-		Zen,
-	}
+    public enum Mode
+    {
+        Classic,
+        GoGreen,
+        Target,
+        Training,
+        TimeAttack,
+        Zen,
+    }
 
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GM = GetNode<GameManager>("/root/GameManager");
-		GM.Score = 0;
-		GM.Lives = 3;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GM = GetNode<GameManager>("/root/GameManager");
+        GM.Score = 0;
+        GM.Lives = 3;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	public void EndGame()
-	{
-		GM.WriteHighScore();
-		GM.SaveData();
-		GM.ChangeSceneToFile("res://Fish the fishes/Scenes/Home.tscn");
-	}
+    public void EndGame()
+    {
+        GM.WriteHighScore();
+        GM.SaveData();
+        GM.ChangeSceneToFile("res://Fish the fishes/Scenes/Home.tscn");
+    }
 
-	private void SpawnFish()
-	{
-		PackedScene FishScene = Fishes[(int)(GD.Randi() % Fishes.Count)];
-		Fish fish = FishScene.Instantiate<Fish>();
+    private void SpawnFish()
+    {
+        PackedScene FishScene = Fishes[(int)(GD.Randi() % Fishes.Count)];
+        Fish fish = FishScene.Instantiate<Fish>();
 
-		bool flip = (GD.Randi() % 2) != 0;
-		Vector2 fishSpawnLocation = new Vector2(flip ? GM.ScreenSize.X + 200 : -200, (float)GD.RandRange(0, GM.ScreenSize.Y));
-		fish.Position = fishSpawnLocation;
-		fish.Flip = flip;
+        bool flip = (GD.Randi() % 2) != 0;
+        Vector2 fishSpawnLocation = new Vector2(flip ? GM.ScreenSize.X + 200 : -200, (float)GD.RandRange(0, GM.ScreenSize.Y));
+        fish.Position = fishSpawnLocation;
+        fish.Flip = flip;
 
-		// Spawn the fish by adding it to the main scene.
-		AddChild(fish);
-	}
+        // Spawn the fish by adding it to the main scene.
+        AddChild(fish);
+    }
 
-	private void Despawn(Node2D body)
-	{
-		if (body is IFishable && (body as IFishable).IsCaught) return;
-		body.QueueFree();
-	}
+    private void Despawn(Node2D body)
+    {
+        if (body is IFishable && (body as IFishable).IsCaught) return;
+        body.QueueFree();
+    }
 
-	private void SpawnTrash()
-	{
-		PackedScene TrashScene = Trashes[(int)(GD.Randi() % Trashes.Count)];
-		Trash trash = TrashScene.Instantiate<Trash>();
-		Vector2 trashSpawnLocation = new Vector2(GD.Randi() % GM.ScreenSize.X, -100);
-		trash.Position = trashSpawnLocation;
-		trash.Velocity = new Vector2(GD.RandRange(-200, 200), 0);
+    private void SpawnTrash()
+    {
+        PackedScene TrashScene = Trashes[(int)(GD.Randi() % Trashes.Count)];
+        Trash trash = TrashScene.Instantiate<Trash>();
+        Vector2 trashSpawnLocation = new Vector2(GD.Randi() % GM.ScreenSize.X, -100);
+        trash.Position = trashSpawnLocation;
+        trash.Velocity = new Vector2(GD.RandRange(-200, 200), 0);
 
-		// Spawn the trash by adding it to the Main scene.
-		AddChild(trash);
-	}
+        // Spawn the trash by adding it to the Main scene.
+        AddChild(trash);
+    }
 }
