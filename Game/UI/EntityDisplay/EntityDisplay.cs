ba1c02a9bd77cede2b52901d29aa4e4e1cae6eeb
Hedgestock <pamarthur.aito@gmail.com>
2025-12-03 22:59:02 +0700

    Added scroll bar for big fishes in compendium

--- Game/UI/EntityDisplay/EntityDisplay.cs
+++ Game/UI/EntityDisplay/EntityDisplay.cs
@@ -2,21 +2,46 @@ using Godot;
 using System;
 using WaffleStock;
 
-public partial class EntityDisplay : ScrollContainer
+public partial class EntityDisplay : Container
 {
     public CharacterBody2D Entity
     {
         set
         {
             var tmp = value.GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
-            Viewport.Size = (Vector2I)(tmp.Rect.Size * tmp.Scale);
-            CustomMinimumSize = new(Math.Min(GetViewportRect().Size.X / 2, Viewport.Size.X), Math.Min(1000, Viewport.Size.Y));
+            EntityViewport.Size = (Vector2I)(tmp.Rect.Size * tmp.Scale);
+            SetEntityDiplaysSize();
             value.Position = tmp.Rect.Size * tmp.Scale / 2;
             ((IFishable)value).IsInDisplay = true;
-            Viewport.AddChild(value);
+            EntityViewport.AddChild(value);
         }
     }
 
     [Export]
-    SubViewport Viewport;
+    ScrollContainer Scroll;
+    [Export]
+    SubViewport EntityViewport;
+
+    [Export]
+    Container ZoomButtons;
+    [Export]
+    Container AnimationButtons;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GetTree().Root.Connect(Viewport.SignalName.SizeChanged, Callable.From(OnScreenResize));
+    }
+
+    private void SetEntityDiplaysSize()
+    {
+        CustomMinimumSize = Scroll.CustomMinimumSize = new(Math.Min(GetViewportRect().Size.X / 2, EntityViewport.Size.X), Math.Min(1000, EntityViewport.Size.Y));
+        GD.Print(Scroll.GetVScrollBar().Visible, Scroll.GetHScrollBar().Visible);
+    }
+
+    private void OnScreenResize()
+    {
+        SetEntityDiplaysSize();
+    }
+
 }
